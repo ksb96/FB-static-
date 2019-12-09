421c9c9e25b9b5d84c4a83ff2dc26296a566058b
kubstar <52975077+kubstar@users.noreply.github.com>
2019-12-09 15:29:27 +0530

    Add files via upload

---
+++ static_FB.js
@@ -0,0 +1,27 @@
+var database = [{username:"Kushal Bhowmick", password:"1234"},
+		{username:"roy", password:"rtsyw"},
+		{username:"harsha", password:"7845"}];
+var newsFeed=[{username:"ritesh",timeline:"i m so tired"},
+	         {username:"roy",timeline:"hey i feeling good"},
+	         {username:"tony", timeline:"I m Iron Man"},
+	          {username:"ultron", timeline:"I dont even know you"}];
+function isUserValid(usrname, password){
+	for(var i = 0; i< database.length; i++){
+	if(database[i].username === username && database[i].password 	=== password){
+	return true;
+	}
+	} return false;
+	}
+
+function signIn(username, password){
+	if(isUserValid(username,password)){
+	console.log(newsFeed);  
+	} else {
+	alert("wrong username & password");
+	}
+	}
+
+var userNamePrompt = prompt("What's ur username?");
+var passwordPrompt = prompt("What ur password?");
+
+signIn(userNamePrompt,passwordPrompt);
